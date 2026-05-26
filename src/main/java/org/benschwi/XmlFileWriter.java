a41a35d9fe997c00e18566eae922206c90eaf944
Schwitzer <schwiete.ben2@gmail.com>
2026-05-26 21:42:19 +0200

    complete rework of XmlFileWriter class

--- src/main/java/org/benschwi/XmlFileWriter.java
+++ src/main/java/org/benschwi/XmlFileWriter.java
@@ -25,18 +25,18 @@ public class XmlFileWriter<A> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(XmlFileWriter.class);
 
-    private final XmlField<A, ?>[] xmlFields;
+    private final XmlNode<A>[] xmlNodes;
 
     /**
      * The instance that represents an XML file writer with a predefined set of attributes from instances of type {@code T}
      *
-     * @param xmlFields the fields whose values appear in each entry of the XML FILE
+     * @param xmlNodes the fields whose values appear in each entry of the XML FILE
      */
     @SafeVarargs
-    public XmlFileWriter(final XmlField<A, ?>...xmlFields) {
-        this.xmlFields = getValidatedXmlFields(xmlFields);
+    public XmlFileWriter(final XmlNode<A>...xmlNodes) {
+        this.xmlNodes = getValidatedXmlFields(xmlNodes);
         LOGGER.debug("Initialized XmlFileWriter instance");
-        LOGGER.debug("The predefined values are {}", Arrays.toString(xmlFields));
+        LOGGER.debug("The predefined values are {}", Arrays.toString(xmlNodes));
     }
 
     /**
@@ -61,25 +61,25 @@ public class XmlFileWriter<A> {
      * @param rootElementName the name of the root element which appears at the top and bottom of the XML file
      * @param comment an optional comment that appears at the top of the XML file
      * @param bufferSize the amount of bytes that the writer stores before writing to a file
-     * @param xmlElements the entries of the XML file to be created
+     * @param xmlEntries the entries of the XML file to be created
      *
      * @throws XMLFileWriterException if one of the values is null or empty
      */
-    public void writeAndCreateXMLFile(final String destinationPath, final String fileName, final String rootElementName, final String comment, final int bufferSize, final Collection<A> xmlElements) {
+    public void writeAndCreateXMLFile(final String destinationPath, final String fileName, final String rootElementName, final String comment, final int bufferSize, final Collection<A> xmlEntries) {
         LOGGER.debug("Start the process of creating and writing a new xml file");
-        validateCreationValues(rootElementName, xmlElements);
+        validateCreationValues(rootElementName, xmlEntries);
         validateBufferSize(bufferSize);
         Path filePath = getValidatedFilePath(destinationPath, fileName);
         LOGGER.debug("The path of the new file : {}", destinationPath);
         LOGGER.debug("The name of the new file : {}", fileName);
         LOGGER.debug("The name of the root element : {}", rootElementName);
         LOGGER.debug("The comment of the file : {}", comment == null ? "No comment given" : comment);
-        LOGGER.debug("The amount of instances to be converted into xml elements : {}", xmlElements.size());
+        LOGGER.debug("The amount of instances to be converted into xml elements : {}", xmlEntries.size());
 
         try(Writer writer = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(filePath), StandardCharsets.UTF_8), bufferSize)) {
             writer.write(getXMLStartContent(comment, rootElementName));
 
-            for(A value : xmlElements) {
+            for(A value : xmlEntries) {
                 if(value != null) {
                     writer.write(getXmlElement(value));
                     LOGGER.debug("Conversion of instance from type {} into an XML element was successful", value.getClass().getSimpleName());
@@ -109,86 +109,74 @@ public class XmlFileWriter<A> {
     }
 
     /**
-     * Get the text representation of {@code xmlElement} with all its child elements and properties being included
+     * Get the text representation of {@code xmlEntry} with all its child elements and properties being included
      *
-     * @param xmlElement the XML element to represent as text
+     * @param xmlEntry the XML element to represent as text
      * @return a text representation of {@code xmlElement}
      */
-    private String getXmlElement(A xmlElement)  {
-        String xmlElementName = xmlElement.getClass().getSimpleName();
-
-        return INDENTATION_LEVEL_1 + getStartTag(xmlElementName) + "\n"
-                + buildXmlElement(xmlElement) +
-                INDENTATION_LEVEL_1 + getEndTag(xmlElementName);
-    }
-
-    /**
-     * Build the text representation of the body of {@code xmlElement}
-     *
-     * @param xmlElement the element to be represented
-     * @return the text body of {@code xmlElement}
-     */
-    private String buildXmlElement(A xmlElement) {
+    private String getXmlElement(A xmlEntry)  {
+        String xmlElementName = xmlEntry.getClass().getSimpleName();
         var stb = new StringBuilder();
 
-        for(XmlField<A, ?> xmlField : xmlFields) {
-            String elementTag = xmlField.name();
-            Object rawValue = xmlField.valueExtractor().apply(xmlElement);
+        stb.append(INDENTATION_LEVEL_1).append(getStartTag(xmlElementName)).append("\n");
 
-            stb.append(INDENTATION_LEVEL_2).append(getStartTag(elementTag));
+        for(XmlNode<A> node : xmlNodes) {
+            stb.append(constructElement(xmlEntry, node, INDENTATION_LEVEL_1));
+        }
 
-            if(rawValue == null && xmlField.nullBehavior() == XmlField.NullBehavior.THROW_EXCEPTION) {
-                throw new XMLFileWriterException("The writing process has failed. The extractor function for the tag with name " + xmlField.name() + " has generated a null value");
+        stb.append(INDENTATION_LEVEL_1).append(getEndTag(xmlElementName));
+        return stb.toString();
+    }
+
+    private String constructElement(Object xmlEntry, XmlNode<?> xmlNode, String indentationLevel) {
+        var stb = new StringBuilder();
+        String tagName = xmlNode.name();
+        switch(xmlNode) {
+            case XmlField<?, ?> xmlField -> {
+                @SuppressWarnings("unchecked")
+                Function<Object, Object> extractor = (Function<Object, Object>) xmlField.valueExtractor();
+                Object rawValue = extractor.apply(xmlEntry);
+
+                stb.append(indentationLevel).append(INDENTATION_LEVEL_1).append(getStartTag(tagName));
+                if(xmlField.hasChildFields()) {
+                    stb.append("\n");
+                    for(XmlNode<?> childNode : xmlField.childFields()) {
+                        stb.append(constructElement(rawValue, childNode, indentationLevel + INDENTATION_LEVEL_1));
+                    }
+                        stb.append(indentationLevel).append(INDENTATION_LEVEL_1);
+                    } else {
+                        stb.append(rawValue != null ? rawValue.toString() : "");
+                    }
+                    stb.append(getEndTag(tagName));
+                }
+                case XmlCollectionField<?, ?> xmlCollectionField -> {
+                    @SuppressWarnings({"rawtypes"})
+                    Function extractor = (Function) xmlCollectionField.valueExtractor();
+                    Collection<?> rawCollection = (Collection<?>) extractor.apply(xmlEntry);
+
+                    stb.append(indentationLevel).append(INDENTATION_LEVEL_2).append(getStartTag(tagName)).append("\n");
+
+                    for(Object value : rawCollection) {
+                        if(xmlCollectionField.hasChildFields()) {
+                            for(XmlNode<?> childNode : xmlCollectionField.childFields()) {
+                                stb.append(constructElement(value, childNode, indentationLevel + INDENTATION_LEVEL_2));
+                            }
+                        } else  {
+                            stb.append(indentationLevel).append(INDENTATION_LEVEL_2).append(getElementWithValue(tagName, value)).append("\n");
+                        }
+                    }
+                    stb.append(getEndTag(tagName)).append("\n");
+                }
             }
-            stb.append(getFullElementConstruct(rawValue, xmlField, INDENTATION_LEVEL_2));
-
-            stb.append(getEndTag(elementTag));
-        }
         return stb.toString();
-    }
-
-    private String getFullElementConstruct(Object rawValue, XmlField<A, ?> xmlField, String currentIndentation) {
-        return rawValue instanceof Collection<?> e ? "\n" + getXmlListElementContent(e, xmlField, currentIndentation) : getXmlElementContent(rawValue, xmlField, currentIndentation);
-    }
-
-    private String getXmlListElementContent(Collection<?> collection, XmlField<A, ?> xmlField, String indentationLevel) {
-        StringBuilder stb = new StringBuilder();
-
-        for(Object instance : collection) {
-            stb.append(indentationLevel).append(INDENTATION_LEVEL_1).append(getStartTag(LIST_ITEM_TAG_NAME));
-            stb.append(getFullElementConstruct(instance, xmlField, indentationLevel + INDENTATION_LEVEL_1));
-            stb.append(getEndTag(LIST_ITEM_TAG_NAME));
-        }
-        stb.append(indentationLevel);
-        return stb.toString();
-    }
-
-    private String getXmlElementContent(Object rawValue, XmlField<?, ?> xmlField, String indentationLevel) {
-        if(rawValue == null) {
-            return "";
-        }
-        return xmlField.hasChildFields() ?  getChildFieldsContentOfElement(rawValue, xmlField, indentationLevel) : rawValue.toString();
-    }
-
-    private String getChildFieldsContentOfElement(Object rawValue, XmlField<?, ?> xmlField, String indentationLevel) {
-        StringBuilder stb = new StringBuilder();
-        stb.append("\n");
-        for(XmlField<?, ?> field : xmlField.childFields()) {
-            @SuppressWarnings("unchecked")
-            Function<Object, Object> extractor = (Function<Object, Object>) field.valueExtractor();
-            Object value = extractor.apply(rawValue);
-
-            stb.append(indentationLevel).append(INDENTATION_LEVEL_1).append(getStartTag(field.name())).append(getXmlElementContent(value, field, indentationLevel)).append(getEndTag(field.name()));
-        }
-        return stb.append(indentationLevel).toString();
     }
 
     @SafeVarargs
-    private XmlField<A, ?>[] getValidatedXmlFields(final XmlField<A, ?>...xmlFields) {
-        if(xmlFields == null || xmlFields.length < 1) {
+    private XmlNode<A>[] getValidatedXmlFields(final XmlNode<A>...xmlNodes) {
+        if(xmlNodes == null || xmlNodes.length < 1) {
             throw new XMLFileWriterException("xmlFields must not be null nor empty. The xml fields determine which values under which name of the given type T appear in the generated file");
         }
-        return xmlFields;
+        return xmlNodes;
     }
 
     private void validateCreationValues(String rootElementName, Collection<A> xmlElements)  {
