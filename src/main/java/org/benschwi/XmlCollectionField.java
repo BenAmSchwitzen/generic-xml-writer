fb734794962da27aa4e441a3f5e85906fd318116
Schwitzer <schwiete.ben2@gmail.com>
2026-05-28 20:04:49 +0200

    add functionality to customize the name of each collection element tag

--- src/main/java/org/benschwi/XmlCollectionField.java
+++ src/main/java/org/benschwi/XmlCollectionField.java
@@ -9,16 +9,18 @@ import java.util.function.Function;
  * The mapping forms an XML list element.
  *
  * @param name the tag name of the XML element
+ * @param elementName the tag name of each element in the collection
  * @param valueExtractor a function that extracts content for this element from a source object of type {@code T}
  * @param childFields array of XML fields. It represents child element mappings. If the array is empty, the XML element is a leaf element. If the array contains at least one field, the XML element is a parent element.
  * @param <A> the type of the source object
  * @param <B> the type of the elements in the collection extracted by the valueExtractor function
  */
-public record XmlCollectionField<A, B>(String name, Function<A, Collection<B>> valueExtractor, XmlField<B, ?>...childFields) implements XmlNode<A> {
+public record XmlCollectionField<A, B>(String name, String elementName, Function<A, Collection<B>> valueExtractor, XmlField<B, ?>...childFields) implements XmlNode<A> {
 
     @SafeVarargs
     public XmlCollectionField {
         Objects.requireNonNull(name, "The name must not be null");
+        Objects.requireNonNull(elementName, "The elementMame must not be null");
         Objects.requireNonNull(valueExtractor, "The extractor function must not be null.");
     }
 
